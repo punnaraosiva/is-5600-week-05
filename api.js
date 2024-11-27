32af86c35c28d74c2e9f1b1d5fa8d62aa8d5a557
punnaraosiva <sivapunnarao.mandadi@slu.edu>
2024-11-27 05:33:18 +0000

    Lab 5 Submission

--- api.js
+++ api.js
@@ -49,9 +49,9 @@ async function getProduct(req, res, next) {
  * @param {object} req 
  * @param {object} res 
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
@@ -60,9 +60,10 @@ async function createProduct(req, res) {
  * @param {object} res
  * @param {function} next
  */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
 }
 
 /**
@@ -71,8 +72,48 @@ async function editProduct(req, res, next) {
  * @param {*} res 
  * @param {*} next 
  */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+
+/**
+ * Create an order
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(orders)
+}
+
+/**
+ * List orders
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
+async function deleteOrder (req, res, next) {
+  const response = await Orders.destroy(req.params.id)
+  res.json(response)
+}
+async function editOrder (req, res, next) {
+  const change = req.body
+  const order = await Orders.edit(req.params.id, change)
+  res.json(order)
 }
 
 module.exports = autoCatch({
@@ -81,5 +122,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  createOrder,
+  listOrders,
+  deleteOrder,
+  editOrder
 });
